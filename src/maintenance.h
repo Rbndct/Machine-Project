8246f43031efe9418f5d3113045836f529481687
Rbee <rbenedict.maagma@gmail.com>
2024-11-05 15:18:34 +0800

    Add maintenance function prototypes and include data_structures.h in maintenance.h
    - Added function prototypes for maintenance functionalities: `maintenanceValidation`, `viewInventory`, `modifyPrice`, and `restockInventory`
    - Included `data_structures.h` for structured data definitions used across maintenance functions

---
+++ src/maintenance.h
@@ -0,0 +1,7 @@
+#include "data_structures.h"
+
+// Maintenance Function Prototypes
+int maintenanceValidation(int *);
+void viewInventory(VendingItem[], int);
+void modifyPrice(VendingItem[], int);
+void restockInventory(VendingItem[], int);
