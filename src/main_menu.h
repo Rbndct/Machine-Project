e30243f0973785d7bb51b3012cf47d405455f18a
Rbee <rbenedict.maagma@gmail.com>
2024-11-05 16:47:35 +0800

    Moved header files (.h) to the include directory for better organization.

--- src/main_menu.h
+++
@@ -1,5 +0,0 @@
-#include "data_structures.h"
-
-int handleMenuSelection(int);
-void processPurchase(VendingItem[], int, float *, CashRegister[], int, UserSelection *, int *);
-void handleMaintenanceOptions(VendingItem[], int);
--- src/maintenance.h
+++
@@ -1,7 +0,0 @@
-#include "data_structures.h"
-
-// Maintenance Function Prototypes
-int maintenanceValidation(int *);
-void viewInventory(VendingItem[], int);
-void modifyPrice(VendingItem[], int);
-void restockInventory(VendingItem[], int);
